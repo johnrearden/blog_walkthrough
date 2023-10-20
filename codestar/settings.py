68a5904cb04dc10e2550a6f0da962275f89854ba
johnrearden <isnocturnal@gmail.com>
2023-10-20 06:19:56 +0000

    Add procfile to project

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-johnrearden-blogwalkthr-fxl1tr2m38s.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['8000-johnrearden-blogwalkthr-fxl1tr2m38s.ws-eu105.gitpod.io', 'johnnys-blog.herokuapp.com', 'localhost']
 
 
 # Application definition
